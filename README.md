4fb2217fc2a423d7e541167d5a88ec9e7dd18b8f
Hien Le <quanghien.a1@gmail.com>
2021-04-17 12:42:51 -0400

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # handwritten-text-recognition
 COMP4102 - Final Project
+
+Athurs: Hien Le (101044264) & Minh Nguyen (101039776)
+Instructor: Rosa Azami
+
+
+
+Instructions:
+  - Ensure that all the necessary packages are installed (e.g. OpenCV, TensorFlow...)
+  - To run the program: `$ python3 main.py`
+  - To test eith different images: in `main.python`, change the test image path (line 60) to one of the test images in the
+      `/images` folder
--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import matplotlib.pyplot as plt
 from letter_recognizer import DataLoader, LetterRecognizer
 import character_separator as character_separator
 
-#define path to an image 
-path = './images/3.png'
-img = cv2.imread(path)
 
-# separated characters from the text
-chars_arr = character_separator.separate_characters(path, (28,28))
 
 # initialize the model and dataset
 LetterRecognizer = LetterRecognizer()
@@ -22,35 +17,47 @@ DataLoader = DataLoader()
 #  print('Evaluating the model...')
 #  LetterRecognizer.evaluate(DataLoader.X_test, DataLoader.Y_test)
 
-print('Making prediction...')
-for i in chars_arr:
-    # make a prediction
-    prediction = LetterRecognizer.predict(i[0], DataLoader.alphabets)
-    print(prediction)
+def predict_characters(img_path):
+    img = cv2.imread(img_path)
+    # separated characters from the text
+    chars_arr = character_separator.separate_characters(img_path, (28,28))
+    print('Making prediction...')
+    for i in chars_arr:
+        # make a prediction
+        prediction = LetterRecognizer.predict(i[0], DataLoader.alphabets)
+        print(prediction)
 
-    # draw a bounding rectangle around the letter
-    x,y,w,h = i[1]
-    pt1 = (x,y)
-    pt2 = (x+w, y+h)
-    green_color = (0,255,0)
-    thickness = 1
+        # draw a bounding rectangle around the letter
+        x,y,w,h = i[1]
+        pt1 = (x,y)
+        pt2 = (x+w, y+h)
+        green_color = (0,255,0)
+        thickness = 1
 
-    cv2.rectangle(img, pt1, pt2, green_color, thickness)
+        cv2.rectangle(img, pt1, pt2, green_color, thickness)
 
-    # draw the prediction of that letter on the image
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    bottomLeftCornerOfText = (x+2,y-5)
-    fontScale = 1
-    red_color = (0,0,255)
-    lineType = 2
+        # draw the prediction of that letter on the image
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (x+2,y-5)
+        fontScale = 1
+        red_color = (0,0,255)
+        lineType = 2
 
-    cv2.putText(img,prediction, bottomLeftCornerOfText,
-        font,
-        fontScale,
-        red_color,
-        lineType)
+        cv2.putText(img,prediction, bottomLeftCornerOfText,
+            font,
+            fontScale,
+            red_color,
+            lineType)
 
 
-# display the final result
-cv2.imshow("Image" , img)
-cv2.waitKey(0)
+    # display the final result
+    #  cv2.imwrite('./result.png', img)
+    cv2.imshow("Image" , img)
+    cv2.waitKey(0)
+
+
+#define path to an image 
+img_path = './images/3.png'
+predict_characters(img_path)
+
+
